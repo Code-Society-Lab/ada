dbd04163c6d060235e59536c1dde2ab1dea00371
penguinboi <simon.roy1211@gmail.com>
2026-04-12 04:50:42 -0400

    Added status command and error handling on command error

--- bot/ada.py
+++ bot/ada.py
@@ -1,7 +1,9 @@
-import time
 import logging
+import time
+from datetime import timedelta
 
-from matrix import Bot, Context, Room
+from matrix import Bot, Context, Room, __version__ as matrix_version
+from matrix.errors import CommandError
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.WARNING)
@@ -18,9 +20,36 @@ async def on_ready():
         await room.send("Hey, I'm back online!")
 
 
-@bot.command("ping")
-async def ping(ctx: Context) -> None:
-    event_time = ctx.event.server_timestamp / 1000
-    latency = round((time.time() - event_time) * 1000)
+@bot.hook
+async def on_command_error(ctx: Context, error: CommandError) -> None:
+    await ctx.reply(f"{error} Run '!help' for help on commands")
 
-    await ctx.reply(f"Pong! {latency}ms")
+
+@bot.command()
+async def ping(ctx: Context) -> None:
+    await ctx.reply(f"Pong! {_get_latency(ctx)}ms")
+
+
+@bot.command()
+async def status(ctx: Context) -> None:
+    human_readable_uptime = str(timedelta(seconds=int(time.time() - bot.start_at)))
+    latency = _get_latency(ctx)
+    extensions = ", ".join(bot.extensions.keys())
+    extension_count = len(bot.extensions)
+    room_count = len(bot.client.rooms)
+
+    await ctx.reply(f"""
+    *“I am in a charming state of confusion.”* - Ada Lovelace
+    ──────────────────────
+    **Uptime**     {human_readable_uptime}
+    **Latency**    {latency}ms
+    **Extensions** {extensions} ({extension_count}) 
+    **Rooms**      {room_count}
+    **matrix.py**  {matrix_version}
+    ──────────────────────
+    """.strip())
+
+
+def _get_latency(ctx: Context) -> float:
+    event_time = ctx.event.server_timestamp / 1000
+    return round((time.time() - event_time) * 1000)
